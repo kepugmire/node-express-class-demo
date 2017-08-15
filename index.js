fe3062e711c418ad6a7b1f43c1bdffcf41214c41
Kellen Pugmire <kepugmire@gmail.com>
2017-08-15 14:09:46 -0600

    NVM

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+
+// =============== TOP - LEVEL MIDDLEWARE =============== //
+
+// CRUD -- REST 
+// CREATE ---------- POST
+// READ ---------- GET
+// UPDATE ---------- PUT
+// DELETE ---------- DELETE
+
+app.
+
+
+
+// =================   ENDPOINTS   ================== //
+
+
+app.listen(port, ()=> console.log("listending on port "+port))
